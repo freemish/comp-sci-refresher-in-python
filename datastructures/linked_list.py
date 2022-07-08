cd8f81bef56e91ed6eaff712528cf355393ed6ee
Molly <molly.gouletas@gmail.com>
2022-07-08 12:02:11 -0700

    Squish a little code duplication

--- datastructures/linked_list.py
+++ datastructures/linked_list.py
@@ -23,36 +23,36 @@ class DoublyLinkedListNode:
         self.val = val
         self.last = last
         self.next = next
-    
+
     def __str__(self) -> str:
         return 'DoublyLinkedListNode({}) <-> {}'.format(self.val, str(self.next))
 
 
-class GraphNode:
-    def __init__(self, val: Any = None, connection_nodes: Optional[List['GraphNode']] = None):
-        self.val = val
-        self.next_nodes = connection_nodes or []
-
-
-def create_linked_list(lst: List[Any]) -> Optional[LinkedListNode]:
+def create_linked_list(
+    lst: List[Any],
+    doubly_linked: bool = False,
+) -> Optional[Union[LinkedListNode, DoublyLinkedListNode]]:
     """Accepts an array of values and outputs head of singly linked list."""
     head = None
     last_node = None
+    node_class = DoublyLinkedListNode if doubly_linked else LinkedListNode
     for i in range(len(lst)):
-        node = LinkedListNode(val=lst[i])
+        node = node_class(val=lst[i])
         if i == 0:
             head = node
             last_node = node
         else:
             last_node.next = node
+            if doubly_linked:
+                node.last = last_node
             last_node = node
     return head
 
 
 def get_array_from_linked_list(
-        linked_list_head: Optional[Union[LinkedListNode, DoublyLinkedListNode]],
-        attr_name_next_node: str = 'next',
-    ) -> List[Any]:
+    linked_list_head: Optional[Union[LinkedListNode, DoublyLinkedListNode]],
+    attr_name_next_node: str = 'next',
+) -> List[Any]:
     """
     Accepts head of linked list and outputs values in a list.
     WARNING! Assumes linked list not circular.
@@ -67,25 +67,9 @@ def get_array_from_linked_list(
     return lst
 
 
-def create_doubly_linked_list(lst: List[Any]) -> DoublyLinkedListNode:
-    """Accepts a list and returns head of a doubly-linked list."""
-    head = None
-    last_node = None
-    for i in range(len(lst)):
-        node = DoublyLinkedListNode(val=lst[i])
-        if i == 0:
-            head = node
-            last_node = node
-        else:
-            last_node.next = node
-            node.last = last_node
-            last_node = node
-    return head
-
-
 def get_last_node_in_linked_list(
-        head: Union[LinkedListNode, DoublyLinkedListNode]
-    ) -> Union[LinkedListNode, DoublyLinkedListNode]:
+    head: Union[LinkedListNode, DoublyLinkedListNode]
+) -> Union[LinkedListNode, DoublyLinkedListNode]:
     """Assumes no circularity."""
     current_node = head
     while current_node.next:
@@ -100,17 +84,22 @@ def main() -> None:
     linked_list_head = create_linked_list(orig_list)
     print('linked list:', linked_list_head)
     new_list = get_array_from_linked_list(linked_list_head)
-    print('list from linked list:', new_list, '\nis equal?', orig_list == new_list)
+    print('list from linked list:', new_list)
+    print('is equal?', orig_list == new_list)
 
     print('Make original list into doubly-linked list...')
-    dlinked_list_head = create_doubly_linked_list(orig_list)
+    dlinked_list_head = create_linked_list(orig_list, doubly_linked=True)
     print('doubly-linked list:', dlinked_list_head)
     new_list = get_array_from_linked_list(dlinked_list_head)
-    print('list from linked list:', new_list, '\nis equal?', orig_list == new_list)
-    last_node = get_last_node_in_linked_list(dlinked_list_head)
-    backwards_dlinked_list = get_array_from_linked_list(last_node, attr_name_next_node='last')
-    print('backwards list from linked list:', backwards_dlinked_list, '\nis equal to reversed orig list?', orig_list[::-1] == backwards_dlinked_list)
+    print('list from linked list:', new_list)
+    print('is equal?', orig_list == new_list)
 
+    backwards_dlinked_list = get_array_from_linked_list(
+        get_last_node_in_linked_list(dlinked_list_head),
+        attr_name_next_node='last',
+    )
+    print('backwards list from doubly-linked list:', backwards_dlinked_list)
+    print('is equal to reversed orig list?', orig_list[::-1] == backwards_dlinked_list)
 
 
 if __name__ == '__main__':
@@ -121,12 +110,12 @@ $ python3 datastructures/linked_list.py
 Starting demo of linked lists...
 original list: ['I', 'am', 'only', 1, 'girl', None]
 linked list: LinkedListNode(I) -> LinkedListNode(am) -> LinkedListNode(only) -> LinkedListNode(1) -> LinkedListNode(girl) -> LinkedListNode(None) -> None
-list from linked list: ['I', 'am', 'only', 1, 'girl', None] 
+list from linked list: ['I', 'am', 'only', 1, 'girl', None]
 is equal? True
 Make original list into doubly-linked list...
 doubly-linked list: DoublyLinkedListNode(I) <-> DoublyLinkedListNode(am) <-> DoublyLinkedListNode(only) <-> DoublyLinkedListNode(1) <-> DoublyLinkedListNode(girl) <-> DoublyLinkedListNode(None) <-> None
-list from linked list: ['I', 'am', 'only', 1, 'girl', None] 
+list from linked list: ['I', 'am', 'only', 1, 'girl', None]
 is equal? True
-backwards list from linked list: [None, 'girl', 1, 'only', 'am', 'I'] 
-is equal? True
+backwards list from doubly-linked list: [None, 'girl', 1, 'only', 'am', 'I']
+is equal to reversed orig list? True
 """
