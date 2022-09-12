761f9bd838e53d37b6fd42b59f8329d9bc76351f
Molly <molly.gouletas@gmail.com>
2022-09-11 23:32:51 -0700

    Add flyweight output

--- designpatterns/flyweight.py
+++ designpatterns/flyweight.py
@@ -17,7 +17,7 @@ class TreeType:
         return self.generate_key(self.name, self.color, self.texture)
     
     def draw(self, x: int, y: int) -> None:
-        print(f'TreeType {self.name} drawn at x {x}, y {y}.')
+        print(f'TreeType {self.get_unique_key()} drawn at x {x}, y {y}.')
 
 
 class Tree:
@@ -73,16 +73,48 @@ def main() -> None:
     )
     print('All flyweights:', tree_factory.list_generated_tree_types())
     print('Initializing 1000 trees for a forest...')
-    trees = []
+    trees: List[Tree] = []
     for x in range(100):
         for y in range(9):
-            trees.append(tree_factory.create_tree('Imaginary', 'red', 'fairydust.jpg', x, y))
+            trees.append(tree_factory.create_tree('Yew', 'red', 'fairydust.jpg', x, y))
     for x in range(10):
         for y in range(10):
             trees.append(tree_factory.create_tree('Cypress', 'beige-green', 'softcypress.jpg', x, y))
 
     print('All flyweights:', tree_factory.list_generated_tree_types())
     print('Length of trees list:', len(trees))
+    print('Drawing a selection of 10 trees...')
+    for tree_index in range(0, 1000, 100):
+        print('Index', tree_index, end=' ')
+        trees[tree_index].draw()
 
 if __name__ == '__main__':
     main()
+
+
+"""
+$ python3 designpatterns/flyweight.py 
+Demonstrating flyweight pattern...
+Initializing 5 different tree types:
+Created and cached new tree type: Cypress__beige-green__softcypress.jpg
+Created and cached new tree type: Willow__sad-green__wispywillow.jpg
+Created and cached new tree type: Gingko__yellow-green__shinyginkgo.jpg
+Created and cached new tree type: Yew__warm-green__yewknow.jpg
+Created and cached new tree type: Oak__dark-green__crispyoak.jpg
+All flyweights: ['Cypress__beige-green__softcypress.jpg', 'Willow__sad-green__wispywillow.jpg', 'Gingko__yellow-green__shinyginkgo.jpg', 'Yew__warm-green__yewknow.jpg', 'Oak__dark-green__crispyoak.jpg']
+Initializing 1000 trees for a forest...
+Created and cached new tree type: Yew__red__fairydust.jpg
+All flyweights: ['Cypress__beige-green__softcypress.jpg', 'Willow__sad-green__wispywillow.jpg', 'Gingko__yellow-green__shinyginkgo.jpg', 'Yew__warm-green__yewknow.jpg', 'Oak__dark-green__crispyoak.jpg', 'Yew__red__fairydust.jpg']
+Length of trees list: 1000
+Drawing a selection of 10 trees...
+Index 0 TreeType Yew__red__fairydust.jpg drawn at x 0, y 0.
+Index 100 TreeType Yew__red__fairydust.jpg drawn at x 11, y 1.
+Index 200 TreeType Yew__red__fairydust.jpg drawn at x 22, y 2.
+Index 300 TreeType Yew__red__fairydust.jpg drawn at x 33, y 3.
+Index 400 TreeType Yew__red__fairydust.jpg drawn at x 44, y 4.
+Index 500 TreeType Yew__red__fairydust.jpg drawn at x 55, y 5.
+Index 600 TreeType Yew__red__fairydust.jpg drawn at x 66, y 6.
+Index 700 TreeType Yew__red__fairydust.jpg drawn at x 77, y 7.
+Index 800 TreeType Yew__red__fairydust.jpg drawn at x 88, y 8.
+Index 900 TreeType Cypress__beige-green__softcypress.jpg drawn at x 0, y 0.
+"""
